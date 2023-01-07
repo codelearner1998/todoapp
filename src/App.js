a916b8152604dd5b0c09a44a5d828bdb4162d92c
MK1998 <86234913+codelearner1998@users.noreply.github.com>
2023-01-07 22:37:48 +0530

    removed extra code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Baby from "./components/baby";
 
 function App() {
+
+  const [inputText , setinputText] = useState("")
+  const [items , setItems] = useState([ ]);
+
+  function handleChange(event) {
+
+  
+
+    const newValue = event.target.value
+
+    setinputText(newValue)
+    
+  }
+
+  function handleClick() {
+
+  setItems(prevItems =>{
+    return [...prevItems ,inputText]
+  })
+
+  setinputText("")
+
+
+
+
+    
+  }
+
+  function deleteItem(id) {
+
+    setItems(prevItems=>{
+
+     return prevItems.filter((item , index)=>{
+       return  id !== index
+     })
+
+    })
+
+   
+  }
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={inputText}/>
+        <button  className="abc" onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+         { items.map((todoItem , index)=>{
+
+             return <Baby
+             key = {index}
+             id = {index}
+             text = {todoItem}
+             Delete = {deleteItem}
+              />
+
+
+          })}
+
+         
+        </ul>
+      </div>
     </div>
   );
 }
